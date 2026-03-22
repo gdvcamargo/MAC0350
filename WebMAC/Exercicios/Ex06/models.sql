92cb1e8b838b6114d32dd1435eb7e81e3ff80dc5
gdvcamargo <zlFastShow@hotmail.com>
2026-03-21 22:53:59 -0300

    [WebMAC]: update exercise 06

--- WebMAC/Exercicios/Ex06/models.sql
+++ WebMAC/Exercicios/Ex06/models.sql
@@ -1,10 +1,20 @@
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+    username_display TEXT NOT NULL,
+    name TEXT NOT NULL,
+);
+
 CREATE TABLE trip (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
     name TEXT NOT NULL,
     destination TEXT NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
-    budget REAL NOT NULL
+    budget REAL NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 CREATE TABLE roadmap (
