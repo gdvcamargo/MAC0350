c4691499a7de00debeb80f0669a409c97dcbf4ae
gdvcamargo <zlFastShow@hotmail.com>
2026-03-25 01:10:23 -0300

    [WebMAC]: update exercise 06

--- WebMAC/Exercicios/Ex06/models.sql
+++ WebMAC/Exercicios/Ex06/models.sql
@@ -1,9 +1,17 @@
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
     username_display TEXT NOT NULL,
-    name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE usersession (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    token TEXT NOT NULL UNIQUE,
+    expires_at DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 CREATE TABLE trip (
@@ -15,37 +23,4 @@ CREATE TABLE trip (
     end_date DATE NOT NULL,
     budget REAL NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(id)
-);
-
-CREATE TABLE roadmap (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    trip_id INTEGER NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    FOREIGN KEY (trip_id) REFERENCES trip(id)
-);
-
-CREATE TABLE roadmap_item (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    date DATE NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
-    roadmap_id INTEGER NOT NULL,
-    FOREIGN KEY (roadmap_id) REFERENCES roadmap(id)
-);
-
-CREATE TABLE place (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    expected_price REAL NOT NULL
-);
-
-CREATE TABLE roadmap_item_place (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    roadmap_item_id INTEGER NOT NULL,
-    place_id INTEGER NOT NULL,
-    
-    FOREIGN KEY (roadmap_item_id) REFERENCES roadmap_item(id),
-    FOREIGN KEY (place_id) REFERENCES place(id)
 );
